a6c6b62eb9f8dedead7325843241dfd532bfa604
Wesley Teubl <mario.sonic1@gmail.com>
2025-04-02 15:12:46 +0200

    Add vacatures

---
+++ webpages/vacatures.php
@@ -0,0 +1,82 @@
+<html>
+
+<head>
+    <title>Welkom bij Karel Bolbloemen B.V. - Error</title>
+    <?php include("../base/head.php") ?>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include("../base/navbar.php") ?>
+
+    <!----Slider---->
+    <?php include("../base/slider.php") ?>
+
+    <section id="vacatures">
+        <div class="container">
+            <h1>Vacature Boekhoudkundig medewerker / Administrateur</h1>
+            <div class="row">
+                <div class="col-12">
+                    <div class="vacatures-content">
+                        <h2>Functieomschrijving</h2>
+                        <p>In deze functie ben je verantwoordelijk voor het verzorgen van de financiële administratie van ons groeiende bedrijf. Je werkt nauw samen met de financial controller en ondersteunt deze met betrouwbare financiële data, voorbereiding voor in- en externe rapportages en ondersteuning bij de totstandkoming van de jaarcijfers. Je verzorgt de BTW-aangifte, boekt de journaalposten en beheert de balansposities.</p>
+                        <h2>Wie wij zoeken heeft:</h2>
+                        <ul>
+                            <li>Een afgeronde MBO-4 opleiding in financieel-administratieve richting (Bedrijfsadministratie, Boekhouden of vergelijkbaar)</li>
+                            <li>Minimaal 2 jaar werkervaring in een vergelijkbare functie</li>
+                            <li>Affiniteit met de agrarische sector (is een pré)</li>
+                            <li>Goede kennis van boekhoudkundige principes en fiscale regelgeving</li>
+                            <li>Ervaring met financiële software (bij voorkeur Exact Globe Next)</li>
+                            <li>Uitstekende beheersing van MS Office, met name Excel</li>
+                            <li>Nauwkeurigheid, integriteit en gestructureerd werken als kernkwaliteiten</li>
+                            <li>Een proactieve en flexibele instelling</li>
+                            <li>Goede communicatieve vaardigheden in het Nederlands en Engels is een pré</li>
+                        </ul>
+                        <h2>Wat wij bieden is:</h2>
+                        <ul>
+                            <li>Een parttime functie met optie tot uitbreiding in de toekomst</li>
+                            <li>Een marktconform salaris, afhankelijk van ervaring en opleiding (functieschaal G)</li>
+                            <li>Arbeidsvoorwaarden volgens de CAO Glastuinbouw</li>
+                            <li>Een informele werksfeer binnen een klein en hecht team</li>
+                            <li>Ruimte voor persoonlijke ontwikkeling en groei</li>
+                            <li>Een moderne werkomgeving in een dynamische sector</li>
+                        </ul>
+                        <h2>Wie wij zijn:</h2>
+                        <p>Karel Bolbloemen is een toonaangevend en ondernemend familiebedrijf in Bovenkarspel, al ruim 65 jaar gespecialiseerd in de teelt en veredeling van hoogwaardige tulpenbollen. Gedreven door vakmanschap, innovatie en duurzaamheid leveren we onze producten aan zowel de Nederlandse als internationale markt.</p>
+                        <br>
+                        <p>Met een hecht team en een no-nonsense mentaliteit werken we aan efficiënte, milieuvriendelijke teeltmethoden, waarbij kwaliteit en klantgerichtheid centraal staan. Korte lijnen, persoonlijk contact en betrouwbaarheid vormen de kern van onze bedrijfscultuur. Bij Karel Bolbloemen combineren we traditie met vernieuwing om de toekomst van de bloembollensector te versterken.</p>
+                        <h2>Heb jij interesse?</h2>
+                        <p>Ben jij de nauwkeurige en betrokken medewerker die wij zoeken? Dan ontvangen wij graag je CV en motivatiebrief vóór 30-4-2025. Je kunt deze sturen naar <a href="mailto:m.korver@karel.nl">m.korver@karel.nl</a>, ter attentie van de heer Manoah Teubl.<br>Voor meer informatie over deze functie kun je contact opnemen met Manoah Teubl via <a href="tel:0228513142">0228-512142</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <div class="navbar-inverse">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p class="navbar-text">
+                            Door <a href="https://www.wesleyteubl.com" style="color: white;" target="_blank" class="navbar-link mr-3">Wesley Teubl</a></p>
+                        <p class="navbar-text">
+                            <a href="https://goo.gl/maps/xxKzAisSK8zAhg4eA" target="_blank">© 2023 Karel Bolbloemen B.V. <span>|</span>
+                                De Gouw 3 <span>|</span>
+                                1611 BS Bovenkarspel <span>|</span></a>
+                            Tel. <a href="tel:0228513142">0228-512142</a> <span>|</span>
+                            Email: <a href="mailto:info@karel.nl" style="color: white">info@karel.nl</a> <span>|</span>
+                            <a href="../webpages/sitemap" style="color: white">Sitemap</a> <span>|</span>
+                            <a href="https://m.facebook.com/profile.php?id=100063553781730" target="_blank"><i class="fa fa-facebook"></i></a> <span>|</span>
+                            <a href="" target="_blank"><i class="fa fa-instagram"></i></a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
